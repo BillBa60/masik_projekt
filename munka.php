08debb7bba525a1052541d95e07dfd9dece60214
BillBa60 <danka.laszlo1@gmail.com>
2023-07-21 10:48:36 +0200

    A többi fájl is push

---
+++ munka.php
@@ -0,0 +1,53 @@
+<?php
+
+// Matematikai műveletek
+//======================
+
+// Összeadás
+//----------
+
+
+$a = 105;
+$b = 22;
+$c = 12;
+$d = 34;
+
+
+
+print $a + $b + c$ + d$;
+
+
+
+
+// Itt a vége ...
+
+// Kivonás  
+//--------
+
+$x = 5;
+$y = 59;
+$z = 370;
+
+print "<br /><br />" . $z - $x - $y . "<br /><br />";
+
+
+
+// Osztas
+//-------
+
+$egyik = 25;
+$masik = 5;
+
+print ($egyik / $masik) . "<br /><br />";>
+
+
+// Szorzás
+//--------
+
+$kicsi = 3;
+$nagy = 23;
+
+print ($kicsi * $nagy) . "<br /><br />";
+
+
+?>
